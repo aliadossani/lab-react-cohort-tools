9f5518fd3dff4dbc359b65ce783bb7cd804f2916
Alia Dossani <dossanialia1408@gmail.com>
2023-12-13 17:27:50 +0100

    4 iterations

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
@@ -8,15 +9,20 @@ import UserProfilePage from "./pages/UserProfilePage";
 function App() {
 
   return (
+
     <div className="App relative z-20 pt-20">
+
       <Navbar />
 
       <div className="pages">
-        <HomePage />
+        <Routes>
+          <Route path='/' element={<HomePage />}> </Route>
+          <Route path='/students/:studentId' element={<StudentDetailsPage />}> </Route>
+          <Route path='/profile' element={<UserProfilePage />}> </Route>
 
-        <StudentDetailsPage />
+          <Route path='*' element={<h1>404 Page not found</h1>}> </Route>
 
-        <UserProfilePage />
+        </Routes>
       </div>
 
     </div>
